c0f7a13e356eb649af033c5823f381d297320b75
Marvin Kang'ethe <marvin.kangethe@andela.com>
2019-08-23 13:32:11 +0300

    Update initialState with data format

--- src/data/initialState.js
+++ src/data/initialState.js
@@ -1,6 +1,86 @@
 export const initialState = {
-  /** Api Status */
+  /**  */
   apiStatus: {
     message: ""
-  }
+  },
+  /** The main current videos */
+  advertisedVideo: {
+    id: '',
+    title: '',
+    description: '',
+    rating: '',
+    releaseDate: '',
+  },
+
+  /** Data structure of Original Content (panel) */
+  originalsVideos: [
+    {
+      id: '',
+      isSeasonedShow: true ,        /** Boolean value */
+      currentWatchingProgress: {
+        season: '',                 /** Both season and episode is nil if hasSeasons returns false */
+        episode: '',
+        pausedAt: ''                /** nil or 0 if not started */
+      },
+      displayImage: ''
+    }
+  ],
+
+  /** Data Structure of Categories Component */
+  availableCategories: [
+    {
+      categoryID: '',
+      displayName: '',
+      videosIDs: []
+    },
+  ],
+
+  /** Data structure of a Video inside a Categories Component */
+  videosByCategory: {
+    categoryID: [
+      {
+        videoID: '',
+        videoName: '',
+        videoRating: '',
+        videoProfileImage: '',
+        userVideoMatchScore: '',
+        topCasts: [{ name: '' }, { name: '' }, { name: '' }]
+      }
+    ]
+  },
+
+  /** Data Structure of video after being expaned in Category Component */
+  videoDetails: {
+    videoOverview: {
+      videoID: '',
+      videoName: '',
+      videoRating: '',
+      videoDescription: '',
+      videoProfileImage: '',
+      userVideoMatchScore: '',
+    },
+    similaTitles: [
+      {
+        videoProfileImage: '',
+        userVideoMatchScore: '',
+        releaseDate: '',
+        videoRating: '',
+        contentRuntime: '',         /** Show number of seasons if series else show shows runtime */
+        videoDescription: ''
+      }
+    ],
+    videoExtraDetails: {
+      videoGenres: [],
+      videoCasts: [],
+      videoCreators: [],
+      videoOtherGenres: []
+    }
+  },
+
+  videoGenres: [
+    {
+      genreID: '',
+      genreName: ''
+    }
+  ]
 };
