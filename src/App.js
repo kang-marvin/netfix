8a672b51fcdb6819bab31e8f903a1281f8fe42fb
Marvin Kang'ethe <marvin.kangethe@andela.com>
2019-08-23 07:18:11 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { BrowserRouter } from "react-router-dom";
+
+require("dotenv").config();
+
+const App = props => {
+
+  const { children } = props;
+
+  return (
+    <BrowserRouter>
+      <div>
+        <div className="sticky-top">
+          <p>Header</p>
+        </div>
+        <div className="container-fluid">
+          {children}
+        </div>
+        <p>Footer</p>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+App.propTypes = {
+  children: PropTypes.object.isRequired
+};
+
+export default App;
